7a41a1c374a62763bf8e789667a91a31056fe304
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2016-04-21 18:33:22 -0500

    linerout: fix some issues in Double Gaussian components

--- src/line_routines_gaussian_fit.cpp
+++ src/line_routines_gaussian_fit.cpp
@@ -269,7 +269,7 @@ XVECTOR Perform_Gaussian_Fit(const XVECTOR & i_vX, const XVECTOR & i_vY, const X
 		}
 	}
 	dDbl_Center = i_vX[uiVar_Min_Idx];
-	dDbl_Amplitude = i_vY[uiVar_Min_Idx] - Multi_Gaussian(i_vX[uiVar_Min_Idx],vA,(void*)i_lpgfpParamters).Get(0);
+	dDbl_Amplitude = dVariance_Min;
 	while (uiVar_Min_Idx > 0 && (i_vY[uiVar_Min_Idx] - Multi_Gaussian(i_vX[uiVar_Min_Idx],vA,(void*)i_lpgfpParamters).Get(0)) > (0.5 * dAmplitude))
 		uiVar_Min_Idx--;
 	vA.Set(0,1.0);
@@ -279,7 +279,7 @@ XVECTOR Perform_Gaussian_Fit(const XVECTOR & i_vX, const XVECTOR & i_vY, const X
 	{
 		dDbl_HWHM = (0.5 * (dHWHM_High + dHWHM_Low));
 		vA.Set(1,dDbl_HWHM); // HWHM
-		dTest = Gaussian(i_vX[uiI],vA,(void*)i_lpgfpParamters)[0];
+		dTest = Gaussian(i_vX[uiVar_Min_Idx],vA,(void*)i_lpgfpParamters)[0];
 		if (dTest > 0.5)
 		{
 			dHWHM_High = dDbl_HWHM;
