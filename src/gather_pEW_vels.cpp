1579828d8c0b572dc939f3c2f097f11e3b22a6cb
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2016-04-27 18:35:26 -0500

    gather_pEW_vels: don't output pEW = 0

--- src/gather_pEW_vels.cpp
+++ src/gather_pEW_vels.cpp
@@ -70,7 +70,7 @@ void Write_Datafile(const char * i_lpszFilename, GROUP i_eGroup, COMPONENT i_eCo
 	for (std::map<unsigned int, std::map<unsigned int, DATA_CONTAINER> >::const_iterator cIterI = i_cFull_Map.begin(); cIterI != i_cFull_Map.end(); cIterI++)
 	{
 		if (i_eGroup != ALL)
-		fprintf(fileFile,"%i",cIterI->first);
+			fprintf(fileFile,"%i",cIterI->first);
 		for (std::map<unsigned int, DATA_CONTAINER>::const_iterator cIterJ = cIterI->second.begin(); cIterJ != cIterI->second.end(); cIterJ++)
 		{
 			if (i_eGroup == ALL)
@@ -95,7 +95,7 @@ void Write_Datafile(const char * i_lpszFilename, GROUP i_eGroup, COMPONENT i_eCo
 						}
 						if (uiI == 0 && lpCtr->m_dVel < 500. ||
 							uiI == 0 && lpCtr->m_dVel > 40000.0 ||
-							uiI != 0 && lpCtr->m_dpEW < 0.)
+							uiI != 0 && lpCtr->m_dpEW <= 0.)
 							fprintf(fileFile,", ");
 						else if (uiI == 0)
 							fprintf(fileFile,", %.0f",lpCtr->m_dVel);
@@ -139,7 +139,7 @@ void Write_Datafile(const char * i_lpszFilename, GROUP i_eGroup, COMPONENT i_eCo
 				}
 				if (i_eComponent == VELOCITY && lpCtr->m_dVel < 500. ||
 					i_eComponent == VELOCITY && lpCtr->m_dVel > 40000.0 ||
-					i_eComponent != VELOCITY && lpCtr->m_dpEW < 0.)
+					i_eComponent != VELOCITY && lpCtr->m_dpEW <= 0.)
 					fprintf(fileFile,", ");
 				else if (i_eComponent == VELOCITY)
 					fprintf(fileFile,", %.0f",lpCtr->m_dVel);
