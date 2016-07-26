49dc7e22deeef3ebeee7fcfaae4db0f0ae56d439
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2016-07-26 16:16:56 -0500

    sf: fix issue if E(B-V) is nan.

--- src/spectra_fit.1.0.cpp
+++ src/spectra_fit.1.0.cpp
@@ -950,7 +950,7 @@ void Deredden(std::vector <specfit::fit> &io_vfitFits)
 	// process each model, and make sure that it has all of the needed info; fill the data points in the vector in the fit class for each one.
 	for (std::vector <specfit::fit>::iterator iterFit = io_vfitFits.begin(); iterFit != io_vfitFits.end(); iterFit++)
 	{
-		if (iterFit->m_dE_BmV != 0.0 && !iterFit->m_vData.empty())
+		if (!std::isnan(iterFit->m_dE_BmV) && iterFit->m_dE_BmV != 0.0 && !iterFit->m_vData.empty())
 		{
 			std::cout << "Dereddending " << iterFit->m_dMJD << " from " << iterFit->m_szSource << " using " << iterFit->m_szInstrument << " with E(B-v) = " << iterFit->m_dE_BmV << std::endl;
 			CCM_dered(iterFit->m_vData, iterFit->m_dE_BmV);
