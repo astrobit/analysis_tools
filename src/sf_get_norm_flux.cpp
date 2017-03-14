84c8f58f0df1e0b03c3e99dca7f65e4877cfb837
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2017-03-14 11:03:54 -0500

    sf: use linerout routines to get normalization fluxes

--- src/sf_get_norm_flux.cpp
+++ src/sf_get_norm_flux.cpp
@@ -10,34 +10,8 @@
 
 void specfit::Get_Normalization_Fluxes(const ES::Spectrum &i_cTarget, const ES::Spectrum &i_cGenerated, const params_range &i_cNorm_Range, double & o_dTarget_Flux, double & o_dGenerated_Flux)
 {
-	unsigned int uiIdx = 0;
-	double	dLuminosity_Target = 0.0, dLuminosity_Synth = 0.0;
-	double	dFit_Delta_Lambda;
-	while (uiIdx < i_cTarget.size() && i_cTarget.wl(uiIdx) < i_cNorm_Range.m_dBlue_WL)
-		uiIdx++;
-	dFit_Delta_Lambda = -i_cTarget.wl(uiIdx);
 
-	while (uiIdx < i_cTarget.size() && i_cTarget.wl(uiIdx) < i_cNorm_Range.m_dRed_WL)
-	{
-		double dDelta_Lambda;
-		if (uiIdx > 0 && uiIdx  < (i_cTarget.size() - 1))
-			dDelta_Lambda = (i_cTarget.wl(uiIdx + 1) - i_cTarget.wl(uiIdx - 1)) * 0.5;
-		else if (uiIdx == 0)
-			dDelta_Lambda = i_cTarget.wl(uiIdx + 1) - i_cTarget.wl(uiIdx);
-		else if (uiIdx == (i_cTarget.size() - 1))
-			dDelta_Lambda = i_cTarget.wl(uiIdx) - i_cTarget.wl(uiIdx - 1);
-
-		dLuminosity_Target += i_cTarget.flux(uiIdx) * dDelta_Lambda;
-		dLuminosity_Synth += i_cGenerated.flux(uiIdx) * dDelta_Lambda;
-		uiIdx++;
-
-	}
-	uiIdx--;
-	dFit_Delta_Lambda += i_cTarget.wl(uiIdx);
-
-	dLuminosity_Target /= dFit_Delta_Lambda; 
-	dLuminosity_Synth /= dFit_Delta_Lambda; 
-	o_dTarget_Flux = dLuminosity_Target * 0.5;// * 1.1;
-	o_dGenerated_Flux = dLuminosity_Synth * 0.5; //dSynth_Normalization;
+	o_dTarget_Flux = Get_Norm_Const(i_cTarget,i_cNorm_Range.m_dBlue_WL,i_cNorm_Range.m_dRed_WL);
+	o_dGenerated_Flux = Get_Norm_Const(i_cGenerated,i_cNorm_Range.m_dBlue_WL,i_cNorm_Range.m_dRed_WL);
 
 }
