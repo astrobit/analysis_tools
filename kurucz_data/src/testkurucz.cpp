fce47cb245dad2463701c424cedf2ab5d244a35a
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2017-11-15 20:46:30 -0600

    add xmath dependency to configure.ac for kurucz_data; add test program to ensure that kurucz data loads

---
+++ kurucz_data/src/testkurucz.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+#include<cstdlib>
+#include <kurucz_data.h>
+
+int main(int i_iArg_Count, const char * i_lpszArg_Values[])
+{
+	Planck_radiation_field rfPlanck(5800.0);
+	//Maxwellian_velocity_function vfMaxwell(5800.0);
+	kurucz_derived_data kddData(20,0,1,rfPlanck,0.0); // Hydrogen -- Ca I and Ca II
+	
+
+	return 0;
+}
