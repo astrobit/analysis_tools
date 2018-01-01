f1a2834a082f7e2601e422ddea16c587ea32a746
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2018-01-01 17:33:02 -0600

    do not include isotope information when loading kurucz data sets. This needs to be handled in some way, but for the time being only use most common / general isotope data

--- kurucz_data/src/kurucz_data.cpp
+++ kurucz_data/src/kurucz_data.cpp
@@ -23,6 +23,7 @@ void kurucz_data::Load_Data(unsigned int i_uiN, unsigned int i_uiMin_Ion, unsign
 			m_uiMax_Ion = m_uiN;
 		for (unsigned int uiI = i_uiMin_Ion; uiI <= m_uiMax_Ion; uiI++)
 		{
+			bool bIsotope_Warning = false;
 			std::ostringstream ossFilename;
 			ossFilename << lpszLA_Data_Path << "/gf";
 	//		ossFilename.fill('0');
@@ -43,10 +44,18 @@ void kurucz_data::Load_Data(unsigned int i_uiN, unsigned int i_uiMin_Ion, unsign
 					Kurucz_Line_Data kldCurr_Line;
 					std::string szLine = lpszLine;
 					kldCurr_Line.Process_Datafile_Line(szLine);
+					
 //						kldCurr_Line.Compute_Z(dRadiation_Temperature_K,dRedshift);
-					vkldIon_Line_Data.push_back(kldCurr_Line);
+					if (kldCurr_Line.m_uiIsotope == 0)//@@TODO - handle isotopes intelligently
+						vkldIon_Line_Data.push_back(kldCurr_Line);
+					else
+						bIsotope_Warning = true;
 				}
 				m_vvkldLine_Data.push_back(vkldIon_Line_Data);
+
+				if (bIsotope_Warning)
+					std::cerr << "Isotope information was contained in the line list of atomic number " << i_uiN << ", ion " << uiI << ". This data was not included when loading the dataset to avoid incorrect identification of levels." << std::endl;
+					
 			}
 			else
 				std::cerr << "Could not open " << ossFilename.str() << std::endl;
