297ad65b1133e167be77515cb4f57bf4e6a923d0
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2018-01-12 16:45:20 -0600

    line routines (radiation): clean up nan checking to generate fewer warnings when nan is detected; make sure that the Lorentz function computes everything using long double.

--- linerout/src/line_routines_radiation.cpp
+++ linerout/src/line_routines_radiation.cpp
@@ -27,11 +27,10 @@ long double Lorentz_Function(const long double & i_dFrequency_Hz, const long dou
 {
 // see http://www.phy.ohiou.edu/~mboett/astro401_fall12/broadening.pdf
 // tested and verified 1 Dec 2016
-	long double dGamma = i_dGamma_Hz * 0.25 / g_XASTRO.k_dpi;
-	long double dDel_f = (i_dFrequency_Hz - i_dFrequency_Reference_Hz); 
-	long double dPhi_f = (dGamma / (dDel_f * dDel_f + dGamma * dGamma) / g_XASTRO.k_dpi);
-	if (std::isnan(dGamma))
-		std::cerr << "Gamma nan\n";
+	long double ldFourth = 0.25L; // make sure that this is loaded as long double
+	long double dGamma(i_dGamma_Hz * ldFourth / g_XASTRO.k_dpi);
+	long double dDel_f(i_dFrequency_Hz - i_dFrequency_Reference_Hz); 
+	long double dPhi_f(dGamma / (dDel_f * dDel_f + dGamma * dGamma) / g_XASTRO.k_dpi);
 	if (dGamma == 0.0)
 		std::cerr << "Gamma zero\n";
 	return dPhi_f;
@@ -41,16 +40,18 @@ long double Line_Energy_Flux_freq(const long double & i_dFrequency_Hz, const voi
 {
 	lef_data * lpldData = (lef_data *) i_lpvData;
 
-	if (std::isnan(i_dFrequency_Hz))
+	if (std::isnan(lpldData->m_dGamma))
+		std::cerr << "Gamma nan\n";
+	if (std::isnan(i_dFrequency_Hz) && !std::isnan(lpldData->m_dGamma))
 		std::cerr << "Frequency nan\n";
 	if (std::isnan(lpldData->m_dRedshift))
 		std::cerr << "redshift nan\n";
 
 	long double dRet = lpldData->m_lpdField->Get_Energy_Flux_freq(i_dFrequency_Hz,lpldData->m_dRedshift);
-	if (std::isnan(dRet))
+	if (std::isnan(dRet) && !std::isnan(i_dFrequency_Hz) && !std::isnan(lpldData->m_dRedshift))
 		std::cerr << "energy flux nan\n";
 	long double dLorentz = Lorentz_Function(i_dFrequency_Hz,lpldData->m_dReference_Frequency_Hz,lpldData->m_dGamma);
-	if (std::isnan(dLorentz))
+	if (std::isnan(dLorentz) && !std::isnan(i_dFrequency_Hz) && !std::isnan(lpldData->m_dReference_Frequency_Hz) && !isnan(lpldData->m_dGamma))
 		std::cerr << "lorentz nan\n";
 	dRet *= dLorentz;
 
